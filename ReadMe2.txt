8566493928a4899319801e0bde5aec2390c9eefc
mehrdady23 <134384460+mehrdady23@users.noreply.github.com>
2023-12-22 09:46:24 -0800

    Add files via upload

---
+++ ReadMe2.txt
@@ -0,0 +1,35 @@
+programs are as follow:
+change_making_dynamic_greedy
+program works into Command Prompt by following command:
+
+python program_name.py return change=n number of denominations=k k-size integer array=""
+
+For Example:
+python change_making_dynamic_greedy.py 57 4 "1 5 10 25"
+which:
+n=57
+k=4
+types of coins: penny=1, nicke=5, dime=10, and quarter=25
+and output is like below:
+
+Dynamic Programming Solution:
+Coins used: [1, 1, 5, 25, 25]
+Number of coins: 5
+Time taken: 0.0 nanoseconds
+
+Greedy Solution:
+Coins used: [25, 25, 5, 1, 1]
+Number of coins: 5
+Time taken: 0.0 nanoseconds
+
+Output
+The program outputs the following information:
+The coins used to make change.
+The Number of coins
+The time taken to compute the solution in nanoseconds.
+
+Testing
+The program has been tested on various test cases, including the US coin system and custom coin systems.
+It has been verified for correctness and performance.
+
+   
--- change_making_bottom_up_dynamic_greedy.py
+++ change_making_bottom_up_dynamic_greedy.py
@@ -1,8 +1,8 @@
 import sys
 import time
 
-# Dynamic Programming Solution
-def dynamic_programming_change(n, k, d):
+# Bottom-up Dynamic Programming solution
+def dynamic_change_making(n, k, d):
     dp = [float('inf')] * (n + 1)
     dp[0] = 0
 
@@ -12,16 +12,15 @@ def dynamic_programming_change(n, k, d):
                 dp[i] = min(dp[i], dp[i - d[j]] + 1)
 
     coins_used = []
-    i = n
-    while i > 0:
+    remaining = n
+    while remaining > 0:
         for j in range(k):
-            if i >= d[j] and dp[i] == dp[i - d[j]] + 1:
+            if remaining >= d[j] and dp[remaining] == dp[remaining - d[j]] + 1:
                 coins_used.append(d[j])
-                i -= d[j]
+                remaining -= d[j]
                 break
 
     return coins_used, dp[n]
-
 
 # Greedy Algorithm solution
 def greedy_change_making(n, k, d):
@@ -38,7 +37,7 @@ def greedy_change_making(n, k, d):
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python change_making_dynamic_greedy.py <n> <k> <d>")
+        print("Usage: python change_making_bottom_up_dynamic_greedy.py <n> <k> <d1 d2 ... dk>")
         sys.exit(1)
 
     n = int(sys.argv[1])
@@ -46,19 +45,17 @@ if __name__ == "__main__":
     d = list(map(int, sys.argv[3].split()))
 
     start_time = time.time()
-    dp_coins, dp_count = dynamic_programming_change(n, k, d)
-    dp_time = time.time() - start_time
+    dynamic_result, dynamic_time = dynamic_change_making(n, k, d)
+    end_time = time.time()
+
+    print("Dynamic Programming Result:")
+    print(dynamic_result)
+    print("Time taken (nanoseconds):", end_time - start_time)
 
     start_time = time.time()
-    greedy_coins, greedy_count = greedy_change_making(n, k, d)
-    greedy_time = time.time() - start_time
+    greedy_result, greedy_time = greedy_change_making(n, k, d)
+    end_time = time.time()
 
-    print("Dynamic Programming Solution:")
-    print("Coins used:", dp_coins)
-    print("Number of coins:", dp_count)
-    print("Time taken:", dp_time, "nanoseconds")
-
-    print("\nGreedy Solution:")
-    print("Coins used:", greedy_coins)
-    print("Number of coins:", greedy_count)
-    print("Time taken:", greedy_time, "nanoseconds")
+    print("\nGreedy Algorithm Result:")
+    print(greedy_result)
+    print("Time taken (nanoseconds):", end_time - start_time)
